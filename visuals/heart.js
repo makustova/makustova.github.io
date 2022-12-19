f95a7077f920377ba79ffa5bc8564d174947978a
Marina Kustova <marina.kustova@youhodler.com>
2022-12-19 23:44:02 +0100

    jaja

--- visuals/heart.js
+++ visuals/heart.js
@@ -6,20 +6,18 @@ export class Heart extends BaseVisual {
   constructor(scene) {
     super(scene);
 
-    this.light = new THREE.PointLight( 0xffffff, 1, 100 );
+    this.light = new THREE.PointLight( 0xffffff, 1 );
     this.light.position.set(0, 0, 0);
     scene.add(this.light);
 
-    this.geometry = new THREE.SphereGeometry(10, 32, 16)
-    this.material = new THREE.MeshPhongMaterial( { color: 0xffffff } );
-    this.material.emissive = 0xffffff
-    this.material.emissiveIntensity = 100
-    this.heart = new THREE.Mesh( this.geometry, this.material );
-    this.scene.add(this.heart);
+    const geometry = new THREE.SphereGeometry(0.5, 32, 16)
+    const material = new THREE.MeshMatcapMaterial( { color: 0xffffff } );
+    const dot = new THREE.Mesh( geometry, material );
+
+    this.scene.add(dot);
   }
 
   move(freqs) {
-    // this.red.intensity = freqs[1] / 1000
-    // this.blue.intensity = freqs[100] / 1000
+    // this.light.intensity = freqs[1] / 1000
   }
 }
