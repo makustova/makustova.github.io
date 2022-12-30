adf3749a5aa5439eb7a87464352901efef239fbd
Marina Kustova <marina.kustova@youhodler.com>
2022-12-30 15:36:58 +0300

    wip

---
+++ bassboosted/bpm.js
@@ -0,0 +1,2 @@
+export const countBpm = (src) => {
+}
--- index.js
+++ index.js
@@ -1,10 +1,45 @@
+import {countBpm} from "./bassboosted/bpm.js";
+
+const audio = document.getElementById('audio')
+
 const submitButton = document.getElementById('submit');
+
+let audioSource;
+let context;
 
 submitButton.onclick = () => {
   const input = document.getElementById('audio-input');
   const blob = input.files[0]
   const blobURL = window.URL.createObjectURL(blob);
-  document.getElementById('audio').setAttribute('src', blobURL)
+  audio.setAttribute('src', blobURL)
+  audio.play()
+  context = new AudioContext();
 
-  document.getElementById('audio').play()
+  console.log(audioSource)
 }
+
+const bassboostedButton = document.getElementById('bassboosted');
+
+const addLowpass = () => {
+  const filter = context.createBiquadFilter();
+  filter.type = "lowpass"; // lowshelf
+  filter.frequency.value = 150;
+  filter.Q.value = 1;
+  filter.connect(context.destination);
+  audioSource.connect(filter);
+}
+
+bassboostedButton.onclick = () => {
+  // countBpm(audioSource);
+  audioSource = context.createMediaElementSource(audio);
+  for (let i=0; i<20; i++) {
+    addLowpass()
+  }
+}
+
+const animate = () => {
+  requestAnimationFrame( animate );
+  // console.log(1)
+}
+
+animate();
