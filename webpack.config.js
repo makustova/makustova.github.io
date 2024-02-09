c236e8f21ef88dd5028626191bc4288371f2e585
Marina Kustova <marina.kustova@youhodler.com>
2024-02-09 16:28:11 +0300

    bp

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+  ],
+  resolve: {
+    extensions: ['.ts', '.js', '.tsx', '.jsx'],
+  }
+};
