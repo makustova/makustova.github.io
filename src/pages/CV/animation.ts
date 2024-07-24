ff0d05adcb1cf0da2fe2ef2f8ac4042dd41486c8
Marina Kustova <marina.kustova@youhodler.com>
2024-07-24 16:02:38 +0200

    bubble

--- src/pages/CV/animation.ts
+++ src/pages/CV/animation.ts
@@ -1,34 +1,83 @@
-const dotsX = 100;
-const dotsY = 100;
+import * as THREE from 'three';
 
-export const draw = (x: number, y: number) => {
-  const dots = new Array(dotsX).fill(new Array(dotsY).fill(1));
+const scene = new THREE.Scene();
 
-  const renderFrame = () => {
-    requestAnimationFrame(renderFrame);
+scene.fog = new THREE.Fog( 0xffffff, 1, 1000 );
 
-    const canvas = document.querySelector("canvas");
-    if (!canvas) {
-      return;
-    }
+const light = new THREE.AmbientLight( 0xffffff ); // soft white light
+scene.add( light );
 
-    const ctx = canvas.getContext("2d");
-    if (!ctx) {
-      return;
-    }
+const pointLight1 = new THREE.PointLight( 0xffffff, 30, 100, 0 );
+pointLight1.position.set( 1, 1, 1 );
+scene.add( pointLight1 );
 
-    ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+const pointLight2 = new THREE.PointLight( 0xffffff, 30, 100, 0 );
+pointLight2.position.set( -2, -2, -2 );
+scene.add( pointLight2 );
 
-    let linesFromAngle = 0;
+scene.background = new THREE.Color( 0x1a1a1a );
+
+// const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const aspect = window.innerWidth / window.innerHeight;
+const frustumSize = 5;
+const camera = new THREE.OrthographicCamera( frustumSize * aspect / - 2, frustumSize * aspect / 2, frustumSize / 2, frustumSize / - 2, 0.1, 100 )
+
+const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setAnimationLoop( animate );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.SphereGeometry( .5, 50, 50 );
+
+const material = new THREE.MeshPhysicalMaterial();
+
+material.color = new THREE.Color( 0xffffff );
+material.roughness = 1;
+material.metalness = 1;
+material.iridescence = 1;
+material.clearcoat = 1;
+material.transparent = true;
+material.opacity = 0.3;
+// material.alphaHash = true;
+material.side = THREE.DoubleSide;
+// material.flatShading = true;
+material.vertexColors = true;
+// material.wireframe = true;
+material.reflectivity = 1;
+material.specularColor = new THREE.Color( 0x000000 );
+material.specularIntensity = 1;
+material.ior = 1.5;
+material.sheenColor = new THREE.Color( 0xffffff );
+
+const bubble = new THREE.Mesh( geometry, material );
+
+// scene.add( bubble1 );
+scene.add( bubble );
+
+camera.position.z = 5;
 
 
+export function animate() {
 
-    ctx.beginPath();
-    ctx.arc(x, y, 20, 0, Math.PI * 2);
-    ctx.fillStyle = "rgba(0, 0, 0, 0.1)";
-    ctx.fill();
-    ctx.closePath();
-  }
+	// bubble2.rotation.x += 0.01;
+	// bubble2.rotation.y += 0.01;
+  // bubble2.rotation.z += 0.01;
 
-  renderFrame();
+  // bubble2.position.x = Math.sin(Date.now() / 1000) * 2;
+  // bubble2.position.y = Math.cos(Date.now() / 1000) * 2;
+  // bubble2.position.z = Math.sin(Date.now() / 1000) * 2;
+
+	renderer.render( scene, camera );
 }
+
+let present = true;
+
+window.addEventListener('click', () => {
+  if (present) {
+    scene.remove(bubble);
+    present = false;
+  } else {
+    scene.add(bubble);
+    present = true;
+  }
+});
--- src/pages/CV/index.tsx
+++ src/pages/CV/index.tsx
@@ -1,28 +1,32 @@
 import * as React from "react";
 import {Link} from "react-router-dom";
 import classes from "./index.module.css";
+import {animate} from "./animation";
 
 export {Cat} from "./Cat";
 export {Dev} from "./Dev";
 export {Mus} from "./Mus";
 
 export const CV: React.FC = () => {
-  const canvasRef = React.useRef<HTMLCanvasElement>(null);
-
   React.useEffect(() => {
-    const canvas = canvasRef.current;
-    if (!canvas) {
-      return;
-    }
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+    animate();
   }, []);
+  // const canvasRef = React.useRef<HTMLCanvasElement>(null);
+
+  // React.useEffect(() => {
+  //   const canvas = canvasRef.current;
+  //   if (!canvas) {
+  //     return;
+  //   }
+  //   canvas.width = window.innerWidth;
+  //   canvas.height = window.innerHeight;
+  // }, []);
 
   return (
     <>
-      <canvas className={classes.canvas} ref={canvasRef}></canvas>
-      <div className={classes.wrapper}>
-        <div className={classes.textBlock}>
+      {/* <canvas className={classes.canvas} ref={canvasRef}></canvas>
+      <div className={classes.wrapper}> */}
+      {/* <div className={classes.textBlock}>
           <h3 className={classes.subtitle}>Hello, my name is</h3>
           <h1 className={classes.title}>Marina</h1>
         </div>
@@ -39,8 +43,8 @@ export const CV: React.FC = () => {
           </Link>
         </div>
 
-        <div className={classes.textBlock}>I make stuff.</div>
-      </div>
+        <div className={classes.textBlock}>I make stuff.</div> */}
+      {/* </div> */}
     </>
   );
 };
